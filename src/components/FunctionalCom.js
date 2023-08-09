26f97889b486d74134485a9d69a25422c00ba8da
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2023-08-09 09:43:37 +0500

    How to create components

---
+++ src/components/FunctionalCom.js
@@ -0,0 +1,10 @@
+// HOW TO CREATE FUNTIONAL COMPONENTS
+function FunctionalCom(){
+    return(
+        <div>
+            <h1>FUNTIONAL COMPONENT</h1>
+        </div>
+    )
+}
+
+export default FunctionalCom;
