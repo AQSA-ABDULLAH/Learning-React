4784005a3d7c2ded277ea46a659b7a4348197b2b
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2023-08-11 00:23:30 +0500

    If Elseif

--- src/App.js
+++ src/App.js
@@ -35,6 +35,7 @@ function App() {
       <HandelingForm/>
       <hr/>
       <ConditionalRendering/>
+      <hr/>
     </div>
   );
 }
--- src/Molecules/ConditionalRendering.js
+++ src/Molecules/ConditionalRendering.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react'
 
 export default function ConditionalRendering() {
-    const [login, setLogin] = useState(true); 
-    // if this is ture welcome aqsa print and if false welcome guest print
+    const [login, setLogin] = useState(2); 
+    // if this is 1 welcome aqsa print and if 2 welcome Merha print and if other than guest print
   return (
     <div>
-        {login? <h1>Welcome Aqsa</h1> : <h1>Welcome Guest</h1>}
+        {login==1? <h1>Welcome Aqsa</h1> : login==2? <h1>Welcome Merha</h1> : <h1>Welcome Guest</h1>}
     </div>
   )
 }
