d86c75fac79b29daa75f32bf52de92939e1f0aa4
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2023-08-11 15:59:06 +0500

    Create Folder Structure

--- src/App.js
+++ src/App.js
@@ -1,49 +1,20 @@
 import './App.css';
-import {useState} from 'react'
-import ClickEventFun from './Functions/ClickEventFun';
-import ClassCom from './components/ClassCom';
-import FunctionalCom from './components/FunctionalCom';
-import PropsWithCom from './components/PropsWithCom';
-import StateInCom from './components/StateInCom';
-import GetInputValue from './Functions/GetInputValue';
-import HideShowToggle from './Functions/HideShowToggle';
-import HandelingForm from './Forms/HandelingForm';
-import ConditionalRendering from './Molecules/ConditionalRendering';
-import FormValidation from './Forms/FormValidation';
-import Constructor from './LifeCyceMethods/Constructor';
-import DidMount from './LifeCyceMethods/DidMount';
+
+// Here are react import
+// import Home from './Home';
+
+
+//Here are redux import
+
 
 function App() {
-  const [data, setData] = useState("Aqsa");
-    function updateData(){
-        setData("Welcome "+data)
-    }
+
   return (
     <div className="App">
-      <FunctionalCom/>
-      <hr/>
-      <ClassCom/>
-      <hr/>
-      <ClickEventFun/>
-      <hr/>
-      <StateInCom/>
-      <hr/>
-      <PropsWithCom name={data} />
-      <button onClick={updateData} >Start</button>
-      <hr/>
-      <GetInputValue/>
-      <hr/>
-      <HideShowToggle/>
-      <hr/>
-      <HandelingForm/>
-      <hr/>
-      <ConditionalRendering/>
-      <hr/>
-      <FormValidation/>
-      <hr/>
-      <Constructor/>
-      <hr/>
-      <DidMount/>
+      {/* Here is React File */}
+      {/* <Home/> */}
+
+      {/* Here is Redux Files */}
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import {useState} from 'react'
+import ClickEventFun from './Functions/ClickEventFun';
+import ClassCom from './components/ClassCom';
+import FunctionalCom from './components/FunctionalCom';
+import PropsWithCom from './components/PropsWithCom';
+import StateInCom from './components/StateInCom';
+import GetInputValue from './Functions/GetInputValue';
+import HideShowToggle from './Functions/HideShowToggle';
+import HandelingForm from './Forms/HandelingForm';
+import ConditionalRendering from './Molecules/ConditionalRendering';
+import FormValidation from './Forms/FormValidation';
+import Constructor from './LifeCyceMethods/Constructor';
+import DidMount from './LifeCyceMethods/DidMount';
+
+export default function Home() {
+    const [data, setData] = useState("Aqsa");
+    function updateData(){
+        setData("Welcome "+data)
+    }
+  return (
+    <div>
+        <FunctionalCom/>
+      <hr/>
+      <ClassCom/>
+      <hr/>
+      <ClickEventFun/>
+      <hr/>
+      <StateInCom/>
+      <hr/>
+      <PropsWithCom name={data} />
+      <button onClick={updateData} >Start</button>
+      <hr/>
+      <GetInputValue/>
+      <hr/>
+      <HideShowToggle/>
+      <hr/>
+      <HandelingForm/>
+      <hr/>
+      <ConditionalRendering/>
+      <hr/>
+      <FormValidation/>
+      <hr/>
+      <Constructor/>
+      <hr/>
+      <DidMount/>
+      <hr/>
+    </div>
+  )
+}
